f76d32c95eee3ebf667c3b4961459e92cfe34c19
Wouter Huysentruit <wouter_huysentruit@hotmail.com>
2018-08-20 23:24:18 +0200

    Split DerAsnIdentifiers in primitive and constructed

--- src/DerConverter/Asn/Enumerations/DerAsnIdentifiers.cs
+++ src/DerConverter/Asn/Enumerations/DerAsnIdentifiers.cs
@@ -2,7 +2,7 @@
 {
     public static class DerAsnIdentifiers
     {
-        public static class Universal
+        public static class Primitive
         {
             public static readonly DerAsnIdentifier Boolean = new DerAsnIdentifier(DerAsnTagClass.Universal, DerAsnEncodingType.Primitive, DerAsnKnownTypeTags.Primitive.Boolean);
             public static readonly DerAsnIdentifier Integer = new DerAsnIdentifier(DerAsnTagClass.Universal, DerAsnEncodingType.Primitive, DerAsnKnownTypeTags.Primitive.Integer);
@@ -11,13 +11,17 @@
             public static readonly DerAsnIdentifier Null = new DerAsnIdentifier(DerAsnTagClass.Universal, DerAsnEncodingType.Primitive, DerAsnKnownTypeTags.Primitive.Null);
             public static readonly DerAsnIdentifier ObjectIdentifier = new DerAsnIdentifier(DerAsnTagClass.Universal, DerAsnEncodingType.Primitive, DerAsnKnownTypeTags.Primitive.ObjectIdentifier);
             public static readonly DerAsnIdentifier Utf8String = new DerAsnIdentifier(DerAsnTagClass.Universal, DerAsnEncodingType.Primitive, DerAsnKnownTypeTags.Primitive.Utf8String);
-            public static readonly DerAsnIdentifier Sequence = new DerAsnIdentifier(DerAsnTagClass.Universal, DerAsnEncodingType.Constructed, DerAsnKnownTypeTags.Constructed.Sequence);
-            public static readonly DerAsnIdentifier Set = new DerAsnIdentifier(DerAsnTagClass.Universal, DerAsnEncodingType.Constructed, DerAsnKnownTypeTags.Constructed.Set);
             public static readonly DerAsnIdentifier PrintableString = new DerAsnIdentifier(DerAsnTagClass.Universal, DerAsnEncodingType.Primitive, DerAsnKnownTypeTags.Primitive.PrintableString);
             public static readonly DerAsnIdentifier TeletexString = new DerAsnIdentifier(DerAsnTagClass.Universal, DerAsnEncodingType.Primitive, DerAsnKnownTypeTags.Primitive.TeletexString);
             public static readonly DerAsnIdentifier Ia5tring = new DerAsnIdentifier(DerAsnTagClass.Universal, DerAsnEncodingType.Primitive, DerAsnKnownTypeTags.Primitive.Ia5tring);
             public static readonly DerAsnIdentifier UtcTime = new DerAsnIdentifier(DerAsnTagClass.Universal, DerAsnEncodingType.Primitive, DerAsnKnownTypeTags.Primitive.UtcTime);
             public static readonly DerAsnIdentifier BmpString = new DerAsnIdentifier(DerAsnTagClass.Universal, DerAsnEncodingType.Primitive, DerAsnKnownTypeTags.Primitive.BmpString);
+        }
+
+        public static class Constructed
+        {
+            public static readonly DerAsnIdentifier Sequence = new DerAsnIdentifier(DerAsnTagClass.Universal, DerAsnEncodingType.Constructed, DerAsnKnownTypeTags.Constructed.Sequence);
+            public static readonly DerAsnIdentifier Set = new DerAsnIdentifier(DerAsnTagClass.Universal, DerAsnEncodingType.Constructed, DerAsnKnownTypeTags.Constructed.Set);
         }
     }
 }
